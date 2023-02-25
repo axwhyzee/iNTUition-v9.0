ad49ef76ccf99c5a3e0c7db4cd50c20bd5222a29
syed0059 <79743238+syed0059@users.noreply.github.com>
2023-02-25 10:35:04 +0000

    changed mainpage

--- frontend/src/components/Mainpage/Mainpage.jsx
+++ frontend/src/components/Mainpage/Mainpage.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
-import { Checkbox, FormControlLabel, FormGroup, List, ListItemButton, ListItemText, Collapse, ListSubheader} from '@mui/material';
+import { Checkbox, FormControlLabel, FormGroup, List, ListItemButton, ListItemText, Collapse, ListSubheader, Paper, Typography, Box} from '@mui/material';
+
 
 
 function Mainpage() {
     const [tasks, setTasks] = useState(["task 1", "task 2", "task 3"]);
+    const [completed, setCompleted] = useState([]);
     const [open, setOpen] = useState(false);
 
     //retrieve from backend
@@ -12,39 +14,55 @@ function Mainpage() {
         const temp = [...tasks];
         temp.splice(temp.indexOf(e.target.value),1);
         setTasks(temp);
+        const tempC = [...completed, e.target.value]
+        setCompleted(tempC)
     }
-
-    const checks = tasks.map(x => {
-        return (
-            <FormControlLabel value={x} control={<Checkbox value={x} checked={false} name={x} onChange={deletetask}/>} label={x} labelPlacement='right'/>
-        )
-       })
 
     const handleClick = () => {
         setOpen(!open);
     }
 
     return (
-        <List sx={{bgcolor: 'background.paper' }}
-        component="nav"
-        subheader={
-          <ListSubheader component="div" id="projects-list-subheader">
-            Projects
-          </ListSubheader>
-        }>
-            <ListItemButton onClick={handleClick}>
-                {/* Make dynamic */}
-                <ListItemText primary="BC2407"/>
-            </ListItemButton>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <FormGroup row={false}>
-                    {checks}
-                </FormGroup>
-            </Collapse>
-            
-        </List>
+        <div>
+            <Box sx={{display:"flex", flexDirection:"row", justifyContent:"space-between"}}>
+                <Paper elevation="2">
+                    <Typography variant="h3">To-do</Typography>
+                    <FormGroup row={false}>
+                        {tasks.map(x => {
+                            return (
+                                <FormControlLabel value={x} control={<Checkbox value={x} checked={false} name={x} onChange={deletetask}/>} label={x} labelPlacement='right'/>
+                            )
+                        })}
+                    </FormGroup>
+                </Paper>
+                <Paper elevation="2">
+                    <Typography variant="h3">Completed</Typography>
+                        {completed.map(x => {
+                            return(
+                                <Typography sx={{textDecoration:"line-through"}}>{x}</Typography>
+                            )
+                        })}
+                </Paper>
+            </Box>
+        </div>
     )
 }
-
+// {/* <List sx={{bgcolor: 'background.paper' }}
+//             component="nav"
+//             subheader={
+//             <ListSubheader component="div" id="projects-list-subheader">
+//                 Projects
+//             </ListSubheader>
+//             }>
+//                 <ListItemButton onClick={handleClick}>
+//                     {/* Make dynamic */}
+//                     <ListItemText primary="BC2407"/>
+//                 </ListItemButton>
+//                 <Collapse in={open} timeout="auto" unmountOnExit>
+//                     <FormGroup row={false}>
+//                         {checks}
+//                     </FormGroup>
+//                 </Collapse>
+//             </List> */}
 export default Mainpage;
 
